a8515a5bf52df581178fbfbdd65a0ec95b3152f7
muralivnv <muralivnv@gmail.com>
2017-12-29 13:52:13 -0600

    uncommented launch_mavros_rosserial.launch

--- bash_scripts/launch_asl_gremlin.sh
+++ bash_scripts/launch_asl_gremlin.sh
@@ -15,9 +15,15 @@ IP=$(hostname -I | awk '{print $1}') # get the IP of the system
 # defining port number of Arduino which controls motors and gets encoder ticks
 arduino_port_num=/dev/ttyACM0
 
+# ROS_MASTER_IP address
+MASTER_IP=$IP
+if [ "$#" == "1" ]; then
+	MASTER_IP=$1
+fi
+
 # Launch the nodes
 export ROS_IP=$IP
-export ROS_MASTER_URI=http://$IP:11311
+export ROS_MASTER_URI=http://$MASTER_IP:11311
 
 roslaunch asl_gremlin_pkg launch_asl_gremlin.launch robot_name:=$robot_name \
 													arduino_port:=$arduino_port_num 
--- bash_scripts/launch_asl_gremlin_split.sh
+++ bash_scripts/launch_asl_gremlin_split.sh
@@ -15,9 +15,15 @@ IP=$(hostname -I | awk '{print $1}') # get the IP of the system
 # defining port number of Arduino which controls motors and gets encoder ticks
 arduino_port_num=/dev/ttyACM0
 
+# ROS_MASTER_IP address
+MASTER_IP=$IP
+if [ "$#" == "1" ]; then
+	MASTER_IP=$1
+fi
+
 # Launch the nodes
 export ROS_IP=$IP
-export ROS_MASTER_URI=http://$IP:11311
+export ROS_MASTER_URI=http://$MASTER_IP:11311
 
 session_name="asl_gremlin_session"
 
